5fc7d5391eeaf8c6de40e29224ec8d14bbe9483e
Aleksandr Sakaev <mastersti111@gmail.com>
2021-01-18 20:41:37 +0500

    add task-1

--- lesson_1/tasks.py
+++ lesson_1/tasks.py
@@ -1,3 +1,8 @@
-
-__author__:'Сакаев Александр Самигуллович'
-    #task 1:
+__author__: 'Сакаев Александр Самигуллович'
+# task 1:
+if __name__ == "__main__":
+    a = 'variable - a'
+    b = 'variable - b'
+    c, d = map(int, input('write two numbers: ').split())
+    y, z = map(str, input('write two string: ').split())
+    print(a, ',', b, ',', c, ',', d, ',', y, ',', z)
