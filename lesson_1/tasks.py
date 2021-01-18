aa1247e95615cece4762f5d314442eaf546ccc86
Aleksandr Sakaev <mastersti111@gmail.com>
2021-01-18 21:15:39 +0500

    rename write to enter

--- lesson_1/tasks.py
+++ lesson_1/tasks.py
@@ -3,6 +3,11 @@ __author__: 'Сакаев Александр Самигуллович'
 if __name__ == "__main__":
     a = 'variable - a'
     b = 'variable - b'
-    c, d = map(int, input('write two numbers: ').split())
-    y, z = map(str, input('write two string: ').split())
+    c, d = map(int, input('enter two numbers: ').split())
+    y, z = map(str, input('enter two string: ').split())
     print(a, ',', b, ',', c, ',', d, ',', y, ',', z)
+
+
+# task 2:
+if __name__ == "__main__":
+    time = int(input('enter the time in second'))
