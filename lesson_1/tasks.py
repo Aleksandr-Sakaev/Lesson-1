d4450aaee30d3b02c4ba35cc0eecdcc52a5b34e1
Aleksandr Sakaev <mastersti111@gmail.com>
2021-01-20 20:19:45 +0500

    add task 2,3,4,5,6

--- lesson_1/tasks.py
+++ lesson_1/tasks.py
@@ -1,13 +1,58 @@
 __author__: 'Сакаев Александр Самигуллович'
 # task 1:
-if __name__ == "__main__":
-    a = 'variable - a'
-    b = 'variable - b'
-    c, d = map(int, input('enter two numbers: ').split())
-    y, z = map(str, input('enter two string: ').split())
-    print(a, ',', b, ',', c, ',', d, ',', y, ',', z)
+
+a = 'variable - a'
+b = 'variable - b'
+c, d = map(int, input('enter two numbers:').split())
+y, z = map(str, input('enter two string: ').split())
+print(a, ',', b, ',', c, ',', d, ',', y, ',', z)
 
 
 # task 2:
-if __name__ == "__main__":
-    time = int(input('enter the time in second'))
+time = int(input('enter second: '))
+hour = ((time // 3600)) % 24
+minute = (time // 60) % 60
+second = time%60
+print(hour, ':', minute, ':', second)
+
+
+# task 3:
+n = input('enter number: ')
+x = n + n
+y = n + n + n
+print(int(n)+int(x)+int(y))
+
+
+# task 4:
+x = int(input('enter integer number: '))
+ls = []
+while x > 10:
+    ls.append(x % 10)
+    x //= 10
+r = max(ls)
+print(r)
+
+# task 5:
+revenue = float(input('inter revenue: '))
+costs = float(input('enter costs: '))
+
+if revenue > costs:
+    ratio = revenue - costs
+    print('Company has a profit. Profitability: ', ratio)
+    count = int(input('enter number of employees: '))
+    profit_workers = ratio / count
+    print('Profit per employees: ', profit_workers)
+elif revenue == costs:
+    print('break-even point')
+else:
+    print('Company has losses')
+
+
+# task 6:
+a = int(input())
+b = int(input())
+days = 1
+while a < b:
+    a = a + (a * 0.1)
+    days += 1
+    print(days)
