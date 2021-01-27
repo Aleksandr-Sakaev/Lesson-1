4068a972181a5f35d129baeead8cec7f241e4139
Aleksandr Sakaev <mastersti111@gmail.com>
2021-01-27 21:09:29 +0500

    add task

--- lesson/tasks.py
+++ lesson/tasks.py
@@ -1,3 +1,19 @@
+__author__: 'Сакаев Александр Самигуллович'
 
-__author__:'Сакаев Александр Самигуллович'
-    #task 1:
+# task 1:
+from sys import argv
+
+
+if __name__=='__main__':
+# def salary(pay_rate, time_worked, bonus):
+#     wage = (pay_rate * time_worked) + bonus
+#     return wage
+#
+#
+# a = int(input('Enter pay rate per hours: '))
+# b = int(input('Enter time worked per hours: '))
+# c = int(input('Enter bonus'))
+#
+# a, b, c = argv
+# salary(a, b, c)
+    print(argv)
